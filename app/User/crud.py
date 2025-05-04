25a8e575de8a06077f6374ab68c86313dfee0dd8
Akashii0 <abdulkid151@gmail.com>
2025-05-04 11:51:09 +0100

    FEAT: ADD method to delete all user tokens in UserRefreshTokenCRUD ♻️

--- app/User/crud.py
+++ app/User/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.common.crud import CRUDBase
 from app.User import models
@@ -11,3 +12,14 @@ class UserCRUD(CRUDBase[models.User]):
 class UserRefreshTokenCRUD(CRUDBase[models.UserRefreshToken]):
     def __init__(self, db: AsyncSession):
         super().__init__(models.UserRefreshToken, db)
+
+    async def delete_tokens(self, user: models.User):
+        """
+        Delete all user tokens
+        """
+
+        # Delete tokens
+        await self.db.execute(delete(self.model).filter_by(user_id=user.id))
+        await self.db.commit()
+
+        return True
